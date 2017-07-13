b3066870772aa6ae64fe27d754eb29bbd8ed20d4
Luis Andrade reuss <landrade@MacBook-Pro-de-Luis.local>
2017-07-13 12:08:04 +0700

    3

--- lab-3-a.rb
+++ lab-3-a.rb
@@ -5,13 +5,19 @@ class Flower
     attr_writer :price
 
     def initialize(:color , :name , :price)
-        @color=color 
-    
+        @color = color 
+        @name = name
+        @price = price
     end
 
+end
+
+    my_flower=Flower.new("red","Rose")
+    my_flower=Flower.price = 11
 
 
- end
+
+ 
 
 
 
