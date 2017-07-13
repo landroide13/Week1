bf567164eefa98bca3b8a533c52271ee5f3d24d4
Luis Andrade reuss <landrade@MacBook-Pro-de-Luis.local>
2017-07-13 13:09:21 +0700

    no attr

--- lab-3-a.rb
+++ lab-3-a.rb
@@ -1,10 +1,31 @@
 
 class Flower
 
-    attr_accessor :color , :name
-    attr_writer :price
+   # attr_accessor :color , :name
+   # attr_writer :price
 
-    def initialize(:color , :name , :price)
+    def name(name)
+       @name=name     
+    end 
+
+    def color(color)
+       @color=color     
+    end 
+
+    def name
+       @name   
+    end
+    
+    def color(color)
+       @color    
+    end 
+
+    def price(price)
+        @price=price
+    end
+
+
+    def initialize(color , name)
         @color = color 
         @name = name
         @price = price
@@ -13,7 +34,10 @@ class Flower
 end
 
     my_flower=Flower.new("red","Rose")
-    my_flower=Flower.price = 11
+    my_flower.price = 11
+
+    you_flower=Flower.new("blue","Tulip")
+    you_flower.price = 25
 
 
 
