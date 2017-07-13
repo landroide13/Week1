704ccf249592d4bee5fe18903cb17f4ac837d6f5
Luis Andrade reuss <landrade@MacBook-Pro-de-Luis.local>
2017-07-13 14:02:20 +0700

    no attr

--- lab-3-a.rb
+++ lab-3-a.rb
@@ -1,47 +1,59 @@
 
 class Flower
-
-   # attr_accessor :color , :name
-   # attr_writer :price
-
-    def name(name)
-       @name=name     
-    end 
-
-    def color(color)
-       @color=color     
-    end 
-
-    def name
-       @name   
-    end
-    
-    def color(color)
-       @color    
-    end 
-
-    def price(price)
-        @price=price
-    end
-
-
-    def initialize(color , name)
-        @color = color 
-        @name = name
-        @price = price
-    end
-
+  
+  # attr_accessor :color , :name
+  # attr_writer :price
+  
+  def name(name)
+    @name=name     
+  end 
+  
+  def color(color)
+    @color=color     
+  end 
+  
+  def name
+    @name   
+  end
+  
+  def color(color)
+    @color    
+  end 
+  
+  def price=(price)
+    @price=price
+  end
+  
+  def price
+    @price
+  end
+  
+  def initialize(color , name)
+    @color = color 
+    @name = name
+  end
+  
 end
 
-    my_flower=Flower.new("red","Rose")
-    my_flower.price = 11
+my_flower=Flower.new("red","Rose" )
+my_flower.price= 11
+puts my_flower.price
 
-    you_flower=Flower.new("blue","Tulip")
-    you_flower.price = 25
+you_flower=Flower.new("blue","Tulip" )
+you_flower.price = 25
 
+puts you_flower
 
+puts my_flower
 
- 
+class Flower
+  puts "Add a mesage"
+  msg=gets.chomp
+
+  def to_sentence(msg)
+    puts "#{msg}"
+  end
+end
 
 
 
