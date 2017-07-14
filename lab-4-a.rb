bfd00a77dcc886c2c4a96b6c1e2ef36832ed09fa
Luis Andrade reuss <landrade@MacBook-Pro-de-Luis.local>
2017-07-14 15:34:09 +0700

    lab4

--- lab-4-a.rb
+++ lab-4-a.rb
@@ -1,5 +1,8 @@
 
 class Item
+
+  attr_accessor :name
+
   def initialize(name, done = false)
     @name = name
     @done = done
@@ -10,18 +13,41 @@ class Item
   end
   
   def mark_done!
-    
+    @done=true
   end
   
   def display
+    
+
   end
   
   def self.new_from_line(line)
+
   end
+
+
+
 end
 
 
+class List
 
+  attr_accessor :items
+
+  def initialize(item = [])
+    @items = item
+    
+  end
+
+  def add(item)
+
+  end
+
+  def display
+    
+  end
+
+end
 
 
 
